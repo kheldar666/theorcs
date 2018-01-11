66d34e01f10e0c0a91dbbc560d1cd35f30d858f8
Martin Papy <martin.papy@gmail.com>
2018-01-11 14:59:17 +0700

    Create Chronicle

--- src/main/java/org/libermundi/theorcs/controllers/secure/ChronicleController.java
+++ src/main/java/org/libermundi/theorcs/controllers/secure/ChronicleController.java
@@ -1,31 +1,47 @@
 package org.libermundi.theorcs.controllers.secure;
 
+import lombok.extern.slf4j.Slf4j;
+import org.libermundi.theorcs.domain.jpa.Picture;
 import org.libermundi.theorcs.domain.jpa.chronicle.Chronicle;
 import org.libermundi.theorcs.domain.jpa.chronicle.News;
 import org.libermundi.theorcs.services.ChronicleService;
+import org.libermundi.theorcs.services.GameService;
 import org.libermundi.theorcs.services.NewsService;
+import org.libermundi.theorcs.services.SecurityService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
 import java.util.List;
 
+@Slf4j
 @Controller
 public class ChronicleController {
 
     private final ChronicleService chronicleService;
 
+    private final GameService gameService;
+
     private final NewsService newsService;
 
-    public ChronicleController(ChronicleService chronicleService, NewsService newsService) {
+    private final SecurityService securityService;
+
+    public ChronicleController(ChronicleService chronicleService, GameService gameService,
+                               NewsService newsService, SecurityService securityService) {
         this.chronicleService = chronicleService;
+        this.gameService = gameService;
         this.newsService = newsService;
+        this.securityService = securityService;
     }
 
     @GetMapping("/secure/chronicle/create")
-    public String create() {
+    public String create(Model model) {
+        model.addAttribute("newChronicle", chronicleService.createNew());
+        model.addAttribute("gameList", gameService.findAll());
         return "/secure/chronicle/create";
     }
 
@@ -41,5 +57,23 @@ public class ChronicleController {
     public String admin(Model model, @PathVariable Chronicle chronicle) {
         return "/secure/chronicle/admin/home";
     }
+
+    @PostMapping(value = "/secure/chronicle/create")
+    public String saveOrUpdate(Model model, @Valid @ModelAttribute("newChronicle") Chronicle chronicle,
+                               BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().forEach(objectError -> {
+                log.error(objectError.toString());
+            });
+            return "/secure/chronicle/admin/news/edit";
+        }
+
+        chronicle.setAdmin(securityService.getCurrentUser());
+        chronicle.setOpenForInscription(Boolean.FALSE);
+
+        chronicleService.save(chronicle);
+        return "redirect:/secure/index";
+    }
+
 
 }
