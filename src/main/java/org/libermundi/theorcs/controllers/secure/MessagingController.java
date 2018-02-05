60439d0d18401377bc75a24172a1e0547439fd9c
Martin Papy <martin.papy@gmail.com>
2018-02-05 17:01:09 +0700

    Some improvments

--- src/main/java/org/libermundi/theorcs/controllers/secure/MessagingController.java
+++ src/main/java/org/libermundi/theorcs/controllers/secure/MessagingController.java
@@ -15,10 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
@@ -87,7 +84,12 @@ public class MessagingController {
 
     @GetMapping("/secure/chronicle/{chronicle}/messaging/write")
     @PreAuthorize("hasPermission(#chronicle, 'read')")
-    public String write(Model model, @PathVariable Chronicle chronicle) {
+    public String write(Model model, @PathVariable Chronicle chronicle,
+            @RequestParam(defaultValue = "false") Boolean multiTo,
+            @RequestParam(defaultValue = "false") Boolean showCcBcc
+                ) {
+        model.addAttribute("multiTo", multiTo);
+        model.addAttribute("showCcBcc", showCcBcc);
         model.addAttribute("newMessage", new MessageForm());
         model.addAttribute("characterList", characterService.getAllCharacters(securityService.getCurrentUser(),chronicle));
         model.addAttribute("contactList", characterService.getAllContacts(securityService.getCurrentUser(),chronicle));
